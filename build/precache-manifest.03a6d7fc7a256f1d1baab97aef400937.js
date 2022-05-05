db4fec9fd487875af77bf2f38b00697073825008
Shankar <Shankar@sigma.com>
2022-05-05 10:15:08 +0530

    update changes

---
+++ build/precache-manifest.03a6d7fc7a256f1d1baab97aef400937.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc425bfd244055a5919aae3ef46ac9a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "808747b9eb9ab843644d",
+    "url": "/static/css/2.c4325bb5.chunk.css"
+  },
+  {
+    "revision": "5743e7c397b9603813bf",
+    "url": "/static/css/main.28d0f1ef.chunk.css"
+  },
+  {
+    "revision": "808747b9eb9ab843644d",
+    "url": "/static/js/2.9a53a936.chunk.js"
+  },
+  {
+    "revision": "7c53eaf7b9f005301a76f746abece0c4",
+    "url": "/static/js/2.9a53a936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5743e7c397b9603813bf",
+    "url": "/static/js/main.ed0ad735.chunk.js"
+  },
+  {
+    "revision": "1e0a5629ac211d21a159",
+    "url": "/static/js/runtime-main.53ffb582.js"
+  },
+  {
+    "revision": "3bde4ff095f553794844c31d365c4425",
+    "url": "/static/media/Sigma-red-Final-logo.3bde4ff0.png"
+  },
+  {
+    "revision": "e519a17bbc84d54124cf5029ccc62285",
+    "url": "/static/media/coming_soon.e519a17b.webp"
+  },
+  {
+    "revision": "695a31606b1abb167b03fb1f6375831d",
+    "url": "/static/media/google.695a3160.svg"
+  }
+]);
